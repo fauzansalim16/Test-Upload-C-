25342e400c321521eb7285881e57bffc45fbf9b1
Fauzan Salim <fauzansalim16@gmail.com>
2022-04-12 14:38:28 +0700

    upload test.cpp

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+    int nilai;
+    cout<<"masukkan nilai yang kamu mau: "<<endl;
+    cin>>nilai;
+    cout<<"Hello world "<<nilai<<endl;
+}
